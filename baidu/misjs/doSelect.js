3115677dca4943454983c516658f08b9cbed2501
archy <chinaarchy@gmail.com>
2018-05-31 11:24:20 +0800

    day31

---
+++ baidu/misjs/doSelect.js
@@ -0,0 +1,74 @@
+
+
+var doSelect = function (flag,changeThing,targetClassName) {
+    var resultTable = document.getElementById('table-wrapper');
+    resultTable.innerText = "";
+    //checkbox的状态判断
+    if (targetClassName == 'all') {
+        toggleCheckbox(flag,changeThing)
+    }else{
+        checkCheckbox(changeThing)
+    }
+    //数据处理
+    var result = [];
+    var countRegion = 0;
+    var countProduct = 3;
+    for (var i = 0; i < selectRegion.length - 1; i++){
+        if (selectRegion[i].checked) {
+            countRegion++;
+            for (var j in sourceData){
+                if (sourceData[j].region == selectRegion[i].value) {
+                    result.push(sourceData[j])
+                }
+            }
+        }
+    }
+    for (var k = 0; k < selectproduct.length - 1; k++) {
+        if (!selectproduct[k].checked) {
+            countProduct--;
+            for (var l in result) {
+                if (result[l].product == selectproduct[k].value) {
+                    result.splice(l, 1)
+                }
+            }
+        }
+    }
+    showTable(result,countProduct,countRegion)
+
+};
+//点击全选的状态切换
+var toggleCheckbox = function (flag,changeThing) {
+    if (flag){
+        if (changeThing == '全选地区'){
+            for (var i = 0; i < selectRegion.length - 1; i++){
+                selectRegion[i].checked = flag
+            }
+        }
+        if (changeThing == '全选商品') {
+            for (var i = 0; i < selectproduct.length - 1; i++){
+                selectproduct[i].checked = flag
+            }
+        }
+    }
+};
+//判断选择的状态
+var checkCheckbox = function(changething){
+    var count = 3;
+    var checkNode = document.querySelector(`input[value='${changething}']`).parentNode.getElementsByTagName('input');
+    for(var i = 0; i < checkNode.length - 1; i++){
+        if (checkNode[i].checked != true){
+            count--;
+            checkNode[3].checked = false
+        }
+    }
+    if (count == 0){
+        for(var i = 0; i < checkNode.length - 1; i++) {
+            if (checkNode[i].value == changething){
+                checkNode[i].checked = true
+            }
+        }
+    }
+    if (count == 3){
+        checkNode[3].checked = true
+    }
+};
---
+++ baidu/misjs/showTable.js
@@ -0,0 +1,66 @@
+/*
+    * 商品为单一时将商品作为第一列，并合并
+    * 地区为单一时将地区为第一列，并合并
+    * 都为一个时，商品第一，地区第二
+    * 都多于一个时，商品第一，地区第二，并合并
+    */
+
+var showTable = function(data,countProduct,countRegion){
+
+    //进行判断
+    if (countProduct <= countRegion){
+        makeTable(countProduct,countRegion,true,data.sort(sortByProduct))
+    } else {
+        makeTable(countRegion,countProduct,false,data.sort(sortByRegion))
+    }
+};
+
+var makeTable = function (numOfFirst, numOfSecond, tableType, data) {
+    var tabNode = document.createElement("table");
+    //插入表头
+    var trHead = tabNode.insertRow();
+
+    //插入数据
+    var count = 0;
+    if (!tableType) {
+        trHead.innerHTML = `<table id="result-table"><tr><td>地区</td><td>产品</td><td>一月</td><td>二月</td><td>三月</td><td>四月</td><td>五月</td><td>六月</td><td>七月</td><td>八月</td><td>九月</td><td>十月</td><td>十一月</td><td>十二月</td></tr>`;
+        for (var x = 0; x < numOfFirst; x++) {
+            for (var y = 0; y < numOfSecond; y++) {
+                var regions = data[count].region;
+                var products = data[count].product;
+                var sales = data[count].sale;
+                var trBody = tabNode.insertRow();
+                var tableFirst = '';
+                if (y == 0) {
+                    tableFirst = `<tr><td id="first-col" rowspan = ${numOfSecond}>${regions}</td><td>${products}</td>`
+                } else {
+                    tableFirst = `<tr><td>${products}</td>`
+                }
+                var t = `<td>${sales[0]}</td><td>${sales[1]}</td><td>${sales[2]}</td><td>${sales[3]}</td><td>${sales[4]}</td><td>${sales[5]}</td><td>${sales[6]}</td><td>${sales[7]}</td><td>${sales[8]}</td><td>${sales[9]}</td><td>${sales[10]}</td><td>${sales[11]}</td></tr>`;
+                trBody.innerHTML = tableFirst + t;
+                count++
+            }
+        }
+    } else {
+        trHead.innerHTML = `<table id="result-table"><tr><td>产品</td><td>地区</td><td>一月</td><td>二月</td><td>三月</td><td>四月</td><td>五月</td><td>六月</td><td>七月</td><td>八月</td><td>九月</td><td>十月</td><td>十一月</td><td>十二月</td></tr>`;
+        for (var x = 0; x < numOfFirst; x++) {
+            for (var y = 0; y < numOfSecond; y++) {
+                var regions = data[count].region;
+                var products = data[count].product;
+                var sales = data[count].sale;
+                var trBody = tabNode.insertRow();
+                var tableFirst = '';
+                if (y == 0) {
+                    tableFirst = `<tr><td id="first-col" rowspan = ${numOfSecond}>${products}</td><td>${regions}</td>`
+                } else {
+                    tableFirst = `<tr><td>${regions}</td>`
+                }
+                var t = `<td>${sales[0]}</td><td>${sales[1]}</td><td>${sales[2]}</td><td>${sales[3]}</td><td>${sales[4]}</td><td>${sales[5]}</td><td>${sales[6]}</td><td>${sales[7]}</td><td>${sales[8]}</td><td>${sales[9]}</td><td>${sales[10]}</td><td>${sales[11]}</td></tr>`;
+                trBody.innerHTML = tableFirst + t;
+                count++
+            }
+        }
+    }
+    //页面插入表格
+    document.getElementById("table-wrapper").appendChild(tabNode)
+};
